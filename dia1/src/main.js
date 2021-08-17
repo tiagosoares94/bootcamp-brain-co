7ba6dcd8b616163b92bd98c7aa371cf820b4b0b9
Tiago Soares <tsoares.sjc@gmail.com>
2021-08-17 17:37:43 -0300

    Exercício 4: Adiciona Event Listner para esconder/mostrar o app

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,10 @@ document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const link = document.querySelector('[data-js="link"]');
+const app = document.querySelector('[data-js="app"]');
+
+link.addEventListener('click', (e) => {
+  e.preventDefault();
+  app.hidden = !app.hidden;
+});
